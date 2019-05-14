e79b78401b13aab53a1b31f9348be8d9a6e96bad
Simon Personal Laptop <cardenas.simon@gmail.com>
2019-05-13 22:49:28 -0700

    Reminder

--- deployer.sh
+++ deployer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-#$(aws ecr get-login --no-include-email --region us-west-2)
-#docker build -t onica-app /home/ec2-user/hello-world/
-#docker tag onica-app:latest 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
-#docker push 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
+echo "Remember to change ecr repo"
+$(aws ecr get-login --no-include-email --region us-west-2)
+docker build -t onica-app /home/ec2-user/hello-world/
+docker tag onica-app:latest 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
+docker push 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
 
 
 while read -r remote_ip
--- helper.sh
+++ helper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Remember to change the ecr url"
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker pull 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
 docker tag 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest onica-app:latest
