b5500efd9ba6f1b597a4efae904d63ae7d5df941
Simon Personal Laptop <cardenas.simon@gmail.com>
2019-05-14 21:54:59 -0700

    Passing repo as argument

--- deployer.sh
+++ deployer.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "Remember to change ecr repo"
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker build -t onica-app /home/ec2-user/hello-world/
-docker tag onica-app:latest 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
-docker push 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
+docker tag onica-app:latest "$1":latest
+docker push "$1":latest
 
 
 while read -r remote_ip
 do
-    ssh -o StrictHostKeyChecking=no -i onica.pem ec2-user@"$remote_ip"  'bash -s' < deployer.sh
+    ssh -o StrictHostKeyChecking=no -i onica.pem ec2-user@"$remote_ip"  'bash -s' < deployer.sh "$1"
 done < hosts
--- helper.sh
+++ helper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 echo "Remember to change the ecr url"
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker pull 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest
-docker tag 224221213629.dkr.ecr.us-west-2.amazonaws.com/onica-app:latest onica-app:latest
+docker pull "$1":latest
+docker tag "$1":latest onica-app:latest
 docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
 docker run -p 80:3000 -d --mount type=bind,source=/var/appData/,target=/var/app/hostname --name onica onica-app:latest
